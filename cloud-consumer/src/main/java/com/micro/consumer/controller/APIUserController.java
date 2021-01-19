3f61d3943bf95ebde2f4587f88c33a48bfb043ac
19043049 <ZYseo111>
2021-01-19 20:15:11 +0800

    优化restTemplate发送服务请求相关代码

--- cloud-consumer/src/main/java/com/micro/consumer/controller/APIUserController.java
+++ cloud-consumer/src/main/java/com/micro/consumer/controller/APIUserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -22,9 +23,10 @@ public class APIUserController {
      * 用户列表
      * @return
      */
+    @ResponseBody
     @RequestMapping("/index")
     public List<User> index(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum){
-        return restTemplate.getForObject(REST_URL_PREFIX+"/user/index/pageNum" + pageNum, List.class);
+        return restTemplate.getForObject(REST_URL_PREFIX+"/user/index?pageNum=" + pageNum, List.class);
     }
 
 }
